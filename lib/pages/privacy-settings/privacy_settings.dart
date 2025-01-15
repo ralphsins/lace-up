d887416e112e433b51ba89ca3f8159b1b4b89aa6
ralphsins <antounralph1@gmail.com>
2025-01-15 18:44:08 +0200

    create privacy settings page

--- lib/pages/privacy-settings/privacy_settings.dart
+++ lib/pages/privacy-settings/privacy_settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:senior_shabeb/widgets/custom_list_tile.dart';
 
 class PrivacySettings extends StatefulWidget {
   const PrivacySettings({super.key});
@@ -10,6 +11,48 @@ class PrivacySettings extends StatefulWidget {
 class _PrivacySettingsState extends State<PrivacySettings> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Privacy and sharing"),
+        elevation: 1,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Lorem ipsum dolor sit amet consectetur. Elit velit in siteu amet justoEros a eget nisl risus ac. Consequat pellentesque amet aenean pellentesque egestas massa.",
+              style: TextStyle(fontWeight: FontWeight.w200),
+            ),
+          ),
+          SizedBox(height: 10),
+          CustomListTile(
+            title: "Request your personal data",
+            subtitle:
+                "Lorem ipsum dolor sit amet consectetur. Elit velit in siteu amet justo.",
+            trailing: Icon(Icons.chevron_right),
+          ),
+          CustomListTile(
+            title: "Delete your account",
+            subtitle:
+                "Lorem ipsum dolor sit amet consectetur. Elit velit in siteu amet justo.",
+            trailing: Icon(Icons.chevron_right),
+          ),
+          CustomListTile(
+            title: "Sharing",
+            subtitle:
+                "Lorem ipsum dolor sit amet consectetur. Elit velit in siteu amet justo.",
+            trailing: Icon(Icons.chevron_right),
+          ),
+          CustomListTile(
+            title: "Service",
+            subtitle:
+                "Lorem ipsum dolor sit amet consectetur. Elit velit in siteu amet justo.",
+            trailing: Icon(Icons.chevron_right),
+          ),
+        ],
+      ),
+    );
   }
 }
